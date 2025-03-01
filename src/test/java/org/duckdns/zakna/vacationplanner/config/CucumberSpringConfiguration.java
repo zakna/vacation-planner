9ca6c110e945317da1a36f65b5c568e5820bacb6
olivier <olivier.vanheuverzwijn@gmail.com>
2025-03-01 18:00:21 +0100

    Add Cucumber Spring configuration and dependencies for testing

---
+++ src/test/java/org/duckdns/zakna/vacationplanner/config/CucumberSpringConfiguration.java
@@ -0,0 +1,12 @@
+package org.duckdns.zakna.vacationplanner.config;
+
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+public class CucumberSpringConfiguration {
+}
