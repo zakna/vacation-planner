5f79f203a0787cf7acf4e4d28f8daa1eb41246b9
olivier <olivier@oliviers-Air-4.fritz.box>
2025-03-01 14:48:03 +0100

    Spring boot Setup

---
+++ src/test/java/org/duckdns/zakna/vacationplanner/VacationPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.duckdns.zakna.vacationplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
